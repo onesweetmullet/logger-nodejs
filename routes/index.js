a33e8328774ad59b361e280bc8efbde84433a15d
Peter Burke <pcburke@gmail.com>
2016-02-17 23:18:28 -0500

    more stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var objectToMarkdownTable = require('../lib/objectToMarkdownTable.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('marked', { title: 'Express' });
 });
 
 module.exports = router;
--- test/objectToMarkdownTable-test.js
+++ test/objectToMarkdownTable-test.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var moment = require('moment');
+var objectToMarkdownTable = require('../lib/objectToMarkdownTable.js');
 
 describe("objectToMarkdown list tests", function() {
 
@@ -9,144 +9,144 @@ describe("objectToMarkdown list tests", function() {
     // build out a test object
     before(function () {
 
-        testObj = { items:[
+        testObj = { "items":[
             {
-                title:'title 1',
-                innerArray: [
+                "title":"title 1",
+                "innerArray": [
                     {
-                        innertitle:'inner title 1',
-                        innerdescription:'inner description 1',
-                        innerInnerArray:[
+                        "innerTitle":"inner title 1",
+                        "innerDescription":"inner description 1",
+                        "innerInnerArray":[
                             {
-                                innerInnerTitle:'inner inner title 1',
-                                innerInnerDescription:'inner inner description 1',
-                                tripleInnerArray:[
+                                "innerInnerTitle":"inner inner title 1",
+                                "innerInnerDescription":"inner inner description 1",
+                                "tripleInnerArray":[
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 1',
-                                        tripleInnerArrayDescription:'triple inner array description 1'
+                                        "tripleInnerArrayTitle":"triple inner array title 1",
+                                        "tripleInnerArrayDescription":"triple inner array description 1"
                                     },
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 2',
-                                        tripleInnerArrayDescription:'triple inner array description 2'
+                                        "tripleInnerArrayTitle":"triple inner array title 2",
+                                        "tripleInnerArrayDescription":"triple inner array description 2"
                                     },
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 3',
-                                        tripleInnerArrayDescription:'triple inner array description 3'
+                                        "tripleInnerArrayTitle":"triple inner array title 3",
+                                        "tripleInnerArrayDescription":"triple inner array description 3"
                                     }
                                 ]
                             },
                             {
-                                innerInnerTitle:'inner inner title 2',
-                                innerInnerDescription:'inner inner description 2',
-                                tripleInnerArray:[
+                                "innerInnerTitle":"inner inner title 2",
+                                "innerInnerDescription":"inner inner description 2",
+                                "tripleInnerArray":[
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 1',
-                                        tripleInnerArrayDescription:'triple inner array description 1'
+                                        "tripleInnerArrayTitle":"triple inner array title 1",
+                                        "tripleInnerArrayDescription":"triple inner array description 1"
                                     },
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 2',
-                                        tripleInnerArrayDescription:'triple inner array description 2'
+                                        "tripleInnerArrayTitle":"triple inner array title 2",
+                                        "tripleInnerArrayDescription":"triple inner array description 2"
                                     },
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 3',
-                                        tripleInnerArrayDescription:'triple inner array description 3'
+                                        "tripleInnerArrayTitle":"triple inner array title 3",
+                                        "tripleInnerArrayDescription":"triple inner array description 3"
                                     }
                                 ]
                             },
                             {
-                                innerInnerTitle:'inner inner title 3',
-                                innerInnerDescription:'inner inner description 3',
-                                tripleInnerArray:[
+                                "innerInnerTitle":"inner inner title 3",
+                                "innerInnerDescription":"inner inner description 3",
+                                "tripleInnerArray":[
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 1',
-                                        tripleInnerArrayDescription:'triple inner array description 1'
+                                        "tripleInnerArrayTitle":"triple inner array title 1",
+                                        "tripleInnerArrayDescription":"triple inner array description 1"
                                     },
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 2',
-                                        tripleInnerArrayDescription:'triple inner array description 2'
+                                        "tripleInnerArrayTitle":"triple inner array title 2",
+                                        "tripleInnerArrayDescription":"triple inner array description 2"
                                     },
                                     {
-                                        tripleInnerArrayTitle:'triple inner array title 3',
-                                        tripleInnerArrayDescription:'triple inner array description 3'
+                                        "tripleInnerArrayTitle":"triple inner array title 3",
+                                        "tripleInnerArrayDescription":"triple inner array description 3"
                                     }
                                 ]
-                            },
+                            }
                         ]
                     },
                     {
-                        innertitle:'inner title 2',
-                        innerdescription:'inner description 2',
-                        innerInnerArray:[
+                        "innerTitle":"inner title 2",
+                        "innerDescription":"inner description 2",
+                        "innerInnerArray":[
                             {
-                                innerInnerTitle:'inner inner title 1',
-                                innerInnerDescription:'inner inner description 1'
+                                "innerInnerTitle":"inner inner title 1",
+                                "innerInnerDescription":"inner inner description 1"
                             },
                             {
-                                innerInnerTitle:'inner inner title 2',
-                                innerInnerDescription:'inner inner description 2'
+                                "innerInnerTitle":"inner inner title 2",
+                                "innerInnerDescription":"inner inner description 2"
                             },
                             {
-                                innerInnerTitle:'inner inner title 3',
-                                innerInnerDescription:'inner inner description 3'
-                            },
+                                "innerInnerTitle":"inner inner title 3",
+                                "innerInnerDescription":"inner inner description 3"
+                            }
                         ]
                     },
                     {
-                        innertitle:'inner title 3',
-                        innerdescription:'inner description 3',
-                        innerInnerArray:[
+                        "innerTitle":"inner title 3",
+                        "innerDescription":"inner description 3",
+                        "innerInnerArray":[
                             {
-                                innerInnerTitle:'inner inner title 1',
-                                innerInnerDescription:'inner inner description 1'
+                                "innerInnerTitle":"inner inner title 1",
+                                "innerInnerDescription":"inner inner description 1"
                             },
                             {
-                                innerInnerTitle:'inner inner title 2',
-                                innerInnerDescription:'inner inner description 2'
+                                "innerInnerTitle":"inner inner title 2",
+                                "innerInnerDescription":"inner inner description 2"
                             },
                             {
-                                innerInnerTitle:'inner inner title 3',
-                                innerInnerDescription:'inner inner description 3'
-                            },
+                                "innerInnerTitle":"inner inner title 3",
+                                "innerInnerDescription":"inner inner description 3"
+                            }
                         ]
                     }
                 ],
-                description:'desc 1'
+                "description":"desc 1"
             },
             {
-                title:'title 2',
-                innerArray: [
+                "title":"title 2",
+                "innerArray": [
                     {
-                        innertitle:'inner title 1',
-                        innerdescription:'inner description 1'
+                        "innerTitle":"inner title 1",
+                        "innerDescription":"inner description 1"
                     },
                     {
-                        innertitle:'inner title 2',
-                        innerdescription:'inner description 2'
+                        "innerTitle":"inner title 2",
+                        "innerDescription":"inner description 2"
                     },
                     {
-                        innertitle:'inner title 3',
-                        innerdescription:'inner description 3'
+                        "innerTitle":"inner title 3",
+                        "innerDescription":"inner description 3"
                     }
                 ],
-                description:'desc 2'
+                "description":"desc 2"
             },
             {
-                title:'title 3',
-                innerArray: [
+                "title":"title 3",
+                "innerArray": [
                     {
-                        innertitle:'inner title 1',
-                        innerdescription:'inner description 1'
+                        "innerTitle":"inner title 1",
+                        "innerDescription":"inner description 1"
                     },
                     {
-                        innertitle:'inner title 2',
-                        innerdescription:'inner description 2'
+                        "innerTitle":"inner title 2",
+                        "innerDescription":"inner description 2"
                     },
                     {
-                        innertitle:'inner title 3',
-                        innerdescription:'inner description 3'
+                        "innerTitle":"inner title 3",
+                        "innerDescription":"inner description 3"
                     }
                 ],
-                description:'desc 3'
+                "description":"desc 3"
             }
         ]};
 
@@ -179,129 +179,8 @@ describe("objectToMarkdown list tests", function() {
     });
 
     it("attempt to convert a json object into a MarkDown table", function(done) {
-        var output = convertObjectToMarkdown(testObj);
+        var output = objectToMarkdownTable.convertObjectToMarkdown(testObj);
         console.log(output);
         done();
     });
 });
-
-var convertArrayToMarkdownTable = function(arr, depth) {
-    var output = '';
-
-    if (!depth) {
-        depth = 0;
-    }
-
-    if (depth > 0) {
-        if (depth > 1)
-            output += '<td>'
-
-        output += '<table>';
-        output += getHeaderRowAsHtml(arr[0]);
-        output += '<tbody>';
-    } else {
-        // generate the header row
-        output += getHeaderRow(arr[0]);
-
-        // generate a separator row
-        output += getRowSeparator(arr[0]);
-    }
-
-    for (var item in arr) {
-
-        if (depth > 0) {
-            output += '<tr>'
-        }
-
-        var obj = arr[item];
-
-        for (var prop in obj) {
-
-            if( Object.prototype.toString.call( obj[prop] ) === '[object Array]' ) {
-                // array - go further down
-                if (depth === 0)
-                    output += ' | ';
-                output += convertArrayToMarkdownTable(obj[prop], depth + 1);
-            } else if (Object.prototype.toString.call( obj[prop] ) === '[object Object]') {
-                // it's an object! loop through the properties
-                output += convertObjectToMarkdown(obj[prop], depth + 1);
-            } else {
-                // not array or an object
-                if (depth > 0)
-                    output += '<td>' + obj[prop] + '</td>';
-                else
-                    output += ' | ' + obj[prop];
-            }
-        }
-
-        // close the row
-        if (depth > 0) {
-            output += '</tr>'
-        } else {
-            output += ' |';
-            output += '\r\n';
-        }
-    }
-
-    if (depth > 0) {
-        output += '</tbody></table>'
-
-        if (depth > 1)
-            output += '</td>';
-    }
-
-    return output;
-};
-
-var convertObjectToMarkdown = function(obj, depth) {
-    var output = '';
-
-    for (var prop in obj) {
-        if( Object.prototype.toString.call( obj[prop] ) === '[object Array]' ) {
-            // array - go further down
-            output += convertArrayToMarkdownTable(obj[prop], depth + 1);
-        } else if (Object.prototype.toString.call( obj[prop] ) === '[object Object]') {
-            // the property is an object... we need to go deeper!
-            output += convertObjectToMarkdown(obj[prop], depth + 1);
-        } else {
-            // not array or an object
-            output += obj[prop];
-        }
-    }
-
-    return output;
-}
-
-var getHeaderRowAsHtml = function(obj) {
-    var output = '<thead><tr>';
-
-    for (var prop in obj) {
-        output += '<td>' + prop + '</td>';
-    }
-
-    output += '</tr></thead>'
-
-    return output;
-}
-
-var getHeaderRow = function(obj) {
-    var output = '';
-
-    for (var prop in obj) {
-        output += '| ' + prop;
-    };
-    output += ' | \r\n';
-
-    return output;
-};
-
-var getRowSeparator = function(obj) {
-    var output = '';
-
-    for (var prop in obj) {
-        output += '|---';
-    };
-    output += ' | \r\n';
-
-    return output;
-};
